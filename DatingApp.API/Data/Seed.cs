0ab40c44f1e068a88881cc72067f5858cfca640b
Luís Sousa <luiscnsousa@outlook.com>
2020-08-18 10:49:08 +0100

    policy based authorization

--- DatingApp.API/Data/Seed.cs
+++ DatingApp.API/Data/Seed.cs
@@ -1,6 +1,7 @@
 namespace DatingApp.API.Data
 {
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
     using System.Threading.Tasks;
     using DatingApp.API.Models;
@@ -9,16 +10,43 @@ namespace DatingApp.API.Data
 
     public class Seed
     {
-        public static async Task SeedUsersAsync(UserManager<User> userManager)
+        public static async Task SeedUsersAsync(UserManager<User> userManager, RoleManager<Role> roleManager)
         {
             if (!userManager.Users.Any())
             {
-                var userData = System.IO.File.ReadAllText("Data/UserSeedData.json");
+                var userData = await File.ReadAllTextAsync("Data/UserSeedData.json");
                 var users = JsonConvert.DeserializeObject<List<User>>(userData);
+
+                var roles = new List<Role>
+                {
+                    new Role { Name = "Member" },
+                    new Role { Name = "Admin" },
+                    new Role { Name = "Moderator" },
+                    new Role { Name = "VIP" }
+                };
+
+                foreach (var role in roles)
+                {
+                    await roleManager.CreateAsync(role);
+                }
                 
                 foreach (var user in users)
                 {
                     await userManager.CreateAsync(user, "password");
+                    await userManager.AddToRoleAsync(user, "Member");
+                }
+
+                var adminUser = new User
+                {
+                    UserName = "Admin"
+                };
+
+                var result = await userManager.CreateAsync(adminUser, "password");
+
+                if (result.Succeeded)
+                {
+                    var admin = await userManager.FindByNameAsync("Admin");
+                    await userManager.AddToRolesAsync(admin, new[] { "Admin", "Moderator" });
                 }
             }
         }
--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -1,18 +1,11 @@
 namespace DatingApp.API
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
     using System.Net;
-    using System.Threading.Tasks;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
-    using Microsoft.AspNetCore.HttpsPolicy;
-    using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
-    using Microsoft.Extensions.Logging;
     using DatingApp.API.Data;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -99,6 +92,13 @@ namespace DatingApp.API
                         ValidateAudience = false
                     };
                 });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+                options.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
+                options.AddPolicy("VipOnly", policy => policy.RequireRole("VIP"));
+            });
             
             services
                 .AddControllers(opt =>
